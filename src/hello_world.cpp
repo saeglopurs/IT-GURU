079e7ba11e569ba930cc39ed68e61d4105ce4d52
saeglopurs <monikaluc.m@gmail.com>
2021-06-12 19:39:05 +0000

    Hello world

---
+++ src/hello_world.cpp
@@ -0,0 +1,16 @@
+#include <iostream>
+
+using namespace std;
+
+// y = main()
+
+// int = integer
+// {} = curly brackets
+// () = parenthesis
+// [] = square brackets
+// "hello" = string
+
+int main()
+{
+    cout << "Duncan\n";
+}
