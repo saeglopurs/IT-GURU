c76360667042a9382f64994b4a2cf925e2261e88
saeglopurs <monikaluc.m@gmail.com>
2021-07-10 21:06:52 +0000

    Revisited things

--- src/hello_world.cpp
+++ src/hello_world.cpp
@@ -2,15 +2,22 @@
 
 using namespace std;
 
+// dupa
 // y = main()
 
-// int = integer
+// int = integer (number)
 // {} = curly brackets
 // () = parenthesis
 // [] = square brackets
 // "hello" = string
 
+int any_name(int a, int b)
+{
+    return a + b;
+}
+
 int main()
 {
     cout << "Hellloooooooooooooooo\n";
+    cout << "Heyka, you're cute :3\n";
 }
